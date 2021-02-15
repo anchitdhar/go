ca76dfecd2aa2f1c952a4b6da15df35f3c8b93cf
Anchit Dhar <anchitdhar@yahoo.com>
2021-02-15 07:45:10 +0530

    Addedtestcases

---
+++ app.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize() {
+	connectionString := fmt.Sprintf("user=%s host=%s password=%s dbname=%s port=%d sslmode=disable", "testgo", "localhost", "testgo", "products", 5432)
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+}
+
+func (a *App) Run(addr string) {}
